fb65b1d2514f6b677acad655694e385a55d9af8e
paresiqbal <thecrowdel@gmail.com>
2023-05-30 23:42:58 +0700

    Add everyting

---
+++ app/auth/Nav.tsx
@@ -0,0 +1,12 @@
+// Next
+import Link from "next/link";
+
+export default async function Nav() {
+  return (
+    <div>
+      <nav>
+        <Link href={"/"}>Dashboard</Link>
+      </nav>
+    </div>
+  );
+}
