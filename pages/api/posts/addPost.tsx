9d5ef1fdd41796c7a13d3b6743d8c9cf5e6fab7b
paresiqbal <thecrowdel@gmail.com>
2023-05-31 22:02:46 +0700

    addPost complete

--- pages/api/posts/addPost.tsx
+++ pages/api/posts/addPost.tsx
@@ -5,6 +5,9 @@ import type { NextApiResponse, NextApiRequest } from "next";
 import { authOptions } from "../auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
 
+// Components
+import prisma from "../../../prisma/client";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -14,6 +17,33 @@ export default async function handler(
 
     // Check user login or not
     if (!session) return res.status(401).json({ message: "Please sign in 😉" });
-    console.log(req.body);
+    const title: string = req.body.title;
+
+    // Get user
+    const prismaUser = await prisma.user.findUnique({
+      where: { email: session?.user?.email },
+    });
+
+    // CHeck title
+    if (title.length > 300)
+      return res.status(403).json({ message: "Please write les then 300 🙏" });
+
+    if (title.length)
+      return res.status(403).json({ message: "Do not leave this empety 😁" });
+
+    // Create post
+    try {
+      const result = await prisma.post.create({
+        data: {
+          title,
+          userId: prismaUser.id,
+        },
+      });
+      res.status(200).json(result);
+    } catch (error) {
+      res
+        .status(403)
+        .json({ message: "Error has occure whilst making a post 😥" });
+    }
   }
 }
