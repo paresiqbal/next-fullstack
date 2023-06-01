1e4cc5817649a88fed5334797fef8c2a1d71c6db
paresiqbal <thecrowdel@gmail.com>
2023-06-01 14:05:41 +0700

    add loading

--- app/components/CreatePost.tsx
+++ app/components/CreatePost.tsx
@@ -11,26 +11,29 @@ export default function CreatePost() {
   const [title, setTitle] = useState("");
   const [isDisabled, setIsDisabled] = useState(false);
 
+  let toastPostID: string;
+
   // Create a post
   const { mutate } = useMutation(
     async (title: string) => await axios.post("/api/posts/addPost", { title }),
     {
       onError: (error) => {
         if (error instanceof AxiosError) {
-          toast.error(error?.response?.data.message);
+          toast.error(error?.response?.data.message, { id: toastPostID });
         }
         setIsDisabled(false);
       },
       onSuccess: (data) => {
         setTitle("");
         setIsDisabled(false);
-        toast.success("Post has been made 🔥");
+        toast.success("Post has been made 🔥", { id: toastPostID });
       },
     }
   );
 
   const submitPost = async (e: React.FormEvent) => {
     e.preventDefault();
+    toastPostID = toast.loading("Creating your post", { id: toastPostID });
     setIsDisabled(true);
     mutate(title);
   };
