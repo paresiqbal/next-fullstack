b264f5cb249c69d95ee873cc630b8f19628715ef
paresiqbal <thecrowdel@gmail.com>
2023-06-02 14:45:30 +0700

    Add MyPost

--- app/dashboard/MyPost.tsx
+++ app/dashboard/MyPost.tsx
@@ -3,3 +3,25 @@
 // Library
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { AuthPosts } from "../types/AuthPosts";
+
+const fetchAuthPosts = async () => {
+  const response = await axios.get("/api/posts/authPosts");
+  return response.data;
+};
+
+export default function MyPosts() {
+  const { data, isLoading } = useQuery<AuthPosts>({
+    queryFn: fetchAuthPosts,
+    queryKey: ["auth-posts"],
+  });
+
+  if (isLoading) return <h1>Post are loading ...</h1>;
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Data</h1>
+    </div>
+  );
+}
