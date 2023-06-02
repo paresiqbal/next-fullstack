6e22809efb57ddc011aa281f44943d1b3bebcac9
paresiqbal <thecrowdel@gmail.com>
2023-06-02 14:34:27 +0700

    Add proflie

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+// Next
+import { getServerSession } from "next-auth";
+
+// Components
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">
+        Welcome back {session.user?.name} !
+      </h1>
+    </div>
+  );
 }
