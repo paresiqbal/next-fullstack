6c99b278040453c3dbc653785fe39387dd784047
paresiqbal <thecrowdel@gmail.com>
2023-06-02 14:49:58 +0700

    complete

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,9 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { redirect } from "next/navigation";
 
+// Pages
+import MyPosts from "./MyPost";
+
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
   if (!session) {
@@ -16,6 +19,7 @@ export default async function Dashboard() {
       <h1 className="text-2xl font-bold">
         Welcome back {session.user?.name} !
       </h1>
+      <MyPosts />
     </div>
   );
 }
