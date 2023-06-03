522cface5ccfc6cc2d45eb41c06b3fb882ce669e
paresiqbal <thecrowdel@gmail.com>
2023-06-03 13:15:04 +0700

    Add templete delet post

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -4,6 +4,9 @@
 import Image from "next/image";
 import { useState } from "react";
 
+// Components
+import Toggle from "./Toggle";
+
 type EditProps = {
   id: string;
   avatar: string;
@@ -24,24 +27,27 @@ export default function EditPost({
   id,
 }: EditProps) {
   return (
-    <div className="bg-white my-8 p-5 rounded-md">
-      <div className="flex gap-2">
-        <Image
-          width={32}
-          height={32}
-          src={avatar}
-          alt="avarat"
-          className="w-6 rounded-full"
-        />
-        <h3 className="text-gray-900 text-sm font-semibold">{name}</h3>
+    <>
+      <div className="bg-white my-8 p-5 rounded-md">
+        <div className="flex gap-2">
+          <Image
+            width={32}
+            height={32}
+            src={avatar}
+            alt="avarat"
+            className="w-6 rounded-full"
+          />
+          <h3 className="text-gray-900 text-sm font-semibold">{name}</h3>
+        </div>
+        <div>
+          <h1 className="ml-8 text-sm break-all">{title}</h1>
+        </div>
+        <div className="flex items-center gap-4 ml-8 my-2">
+          <p className="text-sm font-semibold">{comments?.length} Comments</p>
+          <button className="text-sm font-semibold text-red-500">Delete</button>
+        </div>
       </div>
-      <div>
-        <h1 className="ml-8 text-sm break-all">{title}</h1>
-      </div>
-      <div className="flex items-center gap-4 ml-8 my-2">
-        <p className="text-sm font-semibold">{comments?.length} Comments</p>
-        <button className="text-sm font-semibold text-red-500">Delete</button>
-      </div>
-    </div>
+      <Toggle />
+    </>
   );
 }
